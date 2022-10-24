545b0d3435304799a4657a459f2e01490bf32bbe
Rafael Rebello Vitorino <rrebellov@gmail.com>
2022-10-24 11:23:58 -0300

    adiciona testes e formatação

--- test_ValidaCpf.py
+++ test_ValidaCpf.py
@@ -1,19 +1,27 @@
 from validaCpf import *
 
+
 def testGeraDv1():
-    assert geraDv1('123456789') == 10
+    assert geraDv1("123456789") == 10
+    assert geraDv1("936558900") == 2
+
 
 def testGeraDv2():
-    assert geraDv2('123456789', 10) == 0
+    assert geraDv2("123456789", 10) == 0
+    assert geraDv2("936558900", 2) == 8
+
 
 def testValidaCpf():
-    assert validaNumeroCpf('12345678909') == False
-    assert validaNumeroCpf('12345678900') == True
-    assert validaNumeroCpf('123.456.789-00') == True
-    assert validaNumeroCpf('') == False
+    assert validaNumeroCpf("12345678909") == False
+    assert validaNumeroCpf("12345678900") == True
+    assert validaNumeroCpf("123.456.789-00") == True
+    assert validaNumeroCpf("") == False
+    assert validaNumeroCpf(12345678900) == False
+
 
 def testGeraCpfValido():
     assert validaNumeroCpf(geraCpfValido()) == True
+
 
 def testGeraListaCpf():
     assert len(geraListaCpf(10)) == 10
--- validaCpf.py
+++ validaCpf.py
@@ -1,40 +1,51 @@
-#Crie as seguintes funções: valida_cpf(), gera_cpf_valido() e gera_lista_cpfs()
+# Crie as seguintes funções: valida_cpf(), gera_cpf_valido() e gera_lista_cpfs()
 from random import randint as rint
 
+
 def geraDv1(cpf):
-    soma1 = [int(numero)*(multiplicador+2) for multiplicador,numero in enumerate(cpf[::-1])]
-    dv1 = (sum(soma1))*10%11
+    soma1 = [
+        int(numero) * (multiplicador + 2)
+        for multiplicador, numero in enumerate(cpf[::-1])
+    ]
+    dv1 = (sum(soma1)) * 10 % 11
     return dv1
 
-def geraDv2(cpf,dv1):
-    soma2 = [int(numero)*(multiplicador+3) for multiplicador,numero in enumerate(cpf[::-1])]
-    dv2 = ((sum(soma2)) + (dv1*2)) * 10 % 11
+
+def geraDv2(cpf, dv1):
+    soma2 = [
+        int(numero) * (multiplicador + 3)
+        for multiplicador, numero in enumerate(cpf[::-1])
+    ]
+    dv2 = ((sum(soma2)) + (dv1 * 2)) * 10 % 11
     return dv2
+
 
 def geraDigitosVerificadores(cpf):
 
     if len(cpf) != 9:
         return False
-    
-    dv1 = geraDv1(cpf)
-    dv2 = geraDv2(cpf,dv1)
 
-    dv1 = str(dv1%10)
-    dv2 = str(dv2%10)
+    dv1 = geraDv1(cpf)
+    dv2 = geraDv2(cpf, dv1)
+
+    dv1 = str(dv1 % 10)
+    dv2 = str(dv2 % 10)
 
     digitoVerificador = dv1 + dv2
-    return str(cpf)+str(digitoVerificador)
+    return str(cpf) + str(digitoVerificador)
+
 
 def geraCpfValido():
-    stringGeraCpf = ''
+    stringGeraCpf = ""
 
     for x in range(9):
-        stringGeraCpf+= str(rint(0,9))
+        stringGeraCpf += str(rint(0, 9))
 
     return geraDigitosVerificadores(stringGeraCpf)
 
+
 def validaNumeroCpf(cpf):
-    cpf = cpf.replace('.','').replace('-','')
+    cpf = cpf.replace(".", "").replace("-", "")
     if len(cpf) != 11:
         return False
     cpfVerificado = cpf[:9]
@@ -47,6 +58,3 @@ def geraListaCpf(qtd):
     for x in range(qtd):
         listaCpf.append(geraCpfValido())
     return listaCpf
-
-
-print(validaNumeroCpf(geraCpfValido()))
