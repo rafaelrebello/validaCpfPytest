08836fa1bd618b980bd0804290ca5948568655ff
Rafael Rebello Vitorino <rrebellov@gmail.com>
2022-10-24 10:40:13 -0300

    adicionando gerador de lista de cpf

--- validaCpf.py
+++ validaCpf.py
@@ -1,24 +1,48 @@
-def criarDigitosVerificadores(cpf):
-    cpf = cpf.replace('.','').replace('-','')
-    
-    if len(cpf)!= 9:
-        return False
- 
-    soma1 = [int(numero)*(multiplicador+2) for multiplicador,numero in enumerate(cpf[::-1])]
-    dv1 = sum(soma1)*10%11
-    soma2 = [int(numero)*(multiplicador+3) for multiplicador,numero in enumerate(cpf[::-1])]
-    dv2 = (sum(soma2) + (dv1*2)) * 10 % 11
-    digitoVerificador = str(dv1*10 + dv2)
+#Crie as seguintes funções: valida_cpf(), gera_cpf_valido() e gera_lista_cpfs()
+from random import randint as rint
 
-    return cpf+digitoVerificador
+def geraDigitosVerificadores(cpf):
+
+    if len(cpf) != 9:
+        return False
+    
+    soma1 = [int(numero)*(multiplicador+2) for multiplicador,numero in enumerate(cpf[::-1])]
+    dv1 = (sum(soma1))*10%11
+    
+    
+
+    soma2 = [int(numero)*(multiplicador+3) for multiplicador,numero in enumerate(cpf[::-1])]
+    dv2 = ((sum(soma2)) + (dv1*2)) * 10 % 11
+
+    dv1 = str(dv1%10)
+    dv2 = str(dv2%10)
+
+    digitoVerificador = dv1 + dv2
+    return str(cpf)+str(digitoVerificador)
+
+def geraCpfValido():
+    stringGeraCpf = ''
+
+    for x in range(9):
+        stringGeraCpf+= str(rint(0,9))
+
+    return geraDigitosVerificadores(stringGeraCpf)
 
 def valida_cpf(cpf):
     cpf = cpf.replace('.','').replace('-','')
     if len(cpf) != 11:
         return False
     cpfVerificado = cpf[:9]
-    cpfVerificado = criarDigitosVerificadores(cpfVerificado)
+    cpfVerificado = geraDigitosVerificadores(cpfVerificado)
     print(cpfVerificado)
     return cpfVerificado == cpf
 
-print(criarDigitosVerificadores('108311839'))
+
+def geraListaCpf(qtd):
+    listaCpf = []
+    for x in range(qtd):
+        listaCpf.append(geraCpfValido())
+    return listaCpf
+
+
+print(geraListaCpf(10))
