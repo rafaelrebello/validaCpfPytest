f5e6eee33d3b8a93a0e111d8ad84acb342aec0db
Rafael Rebello Vitorino <rrebellov@gmail.com>
2022-10-25 10:59:28 -0300

    adiciona teste com inteiro

--- test_ValidaCpf.py
+++ test_ValidaCpf.py
@@ -11,12 +11,12 @@ def testGeraDv2():
     assert geraDv2("936558900", 2) == 8
 
 
-def testValidaCpf():
+def testvalidaNumeroCpf():
     assert validaNumeroCpf("12345678909") == False
     assert validaNumeroCpf("12345678900") == True
     assert validaNumeroCpf("123.456.789-00") == True
     assert validaNumeroCpf("") == False
-    assert validaNumeroCpf(12345678900) == False
+    assert validaNumeroCpf(12345678900) == True
 
 
 def testGeraCpfValido():
